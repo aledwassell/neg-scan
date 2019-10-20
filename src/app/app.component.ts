a3799ea7e2a71d4bfeb76829bb66ed115f5e8582
aledwassell <aledwassell@googlemail.com>
2019-10-20 18:16:43 +0100

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
 import {Component, ViewChild, ElementRef} from '@angular/core';
 
-import {UiComponent} from './ui/ui.component';
-
 @Component({
   selector: 'background-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  constructor(){}
 }
